8474fe697deecf893bbb008b275061b70c836bec
talarisulochana <talarisulochana123@gmail.com>
2024-11-12 20:01:05 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,31 @@
+
+var age=22;
+if(age > 18){
+    console.log("eligible for vote")
+}else{
+    console.log("not eligible")
+}
+
+var weather="rainy";
+if(weather==="rainy"){
+    console.log("its pouring outside")
+}else{
+    console.log("not raining")
+}
+
+var marks=76;
+if(marks>90){
+    console.log("A+")
+}else if(marks>81 && marks<90){
+    console.log("A")
+}else if(marks>71 && marks<80){
+    console.log("B+")
+}else if(marks>61 && marks<70){
+    console.log("C+")
+}else if(marks>51 && marks<60){
+    console.log("C")
+}else if(marks>40 && marks<50){
+    console.log("D")
+}else if(marks>50){
+console.log("fail")
+}
